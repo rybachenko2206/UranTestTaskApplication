3714893cbde517c8fc3daf379e1e779e5d81deda
rrybachenko <rrybachenko@grossum.com>
2016-12-28 15:23:25 +0200

    added SWTableViewCell with cocoapods, added some icons

---
+++ UranTestTask/Classes/Views/FileTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  FileTableViewCell.h
+//  UranTestTask
+//
+//  Created by Roman Rybachenko on 12/28/16.
+//  Copyright © 2016 Roman Rybachenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FileTableViewCell : UITableViewCell
+
+@end
---
+++ UranTestTask/Classes/Views/FileTableViewCell.m
@@ -0,0 +1,20 @@
+//
+//  FileTableViewCell.m
+//  UranTestTask
+//
+//  Created by Roman Rybachenko on 12/28/16.
+//  Copyright © 2016 Roman Rybachenko. All rights reserved.
+//
+
+#import "FileTableViewCell.h"
+
+@implementation FileTableViewCell
+
+
+#pragma mark - Static methods
+
++ (NSString *)cellIdentifier {
+    return NSStringFromClass(self);
+}
+
+@end
